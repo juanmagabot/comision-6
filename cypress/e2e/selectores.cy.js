ad66a93e124d30b7495f9a8ae3d3d92d114720a3
Fabrizio Otranto <Fabryotranto@gmail.com>
2023-02-21 21:33:10 -0300

    clase-3

--- cypress/e2e/selectores.cy.js
+++ cypress/e2e/selectores.cy.js
@@ -14,33 +14,53 @@ describe('Selectores', () => {
         cy.get('input[class="chakra-input password css-1ekf6i8"]').should('exist');
     });
 
-    it('Utilizando el metodo find', () =>{
+    it('Utilizando el metodo find', () => {
         cy.visit('');
         cy.get('fieldset').find('input').should('exist')
     })
 
-    it('Utilizando el metodo children', () =>{
+    it('Utilizando el metodo children', () => {
         cy.visit('');
         cy.get('label').children('input[value="Male"]').should('exist')
     })
 
-    it('Utilizando el metodo parent', () =>{
+    it('Utilizando el metodo parent', () => {
         cy.visit('');
         cy.get('input[value="Male"]').parent('label').should('exist');
     });
 
-    it('Utilizando el metodo sibling', () =>{
+    it('Utilizando el metodo sibling', () => {
         cy.visit('');
         cy.get('input#user').siblings('label').should('exist');
     });
 
-    it('Utilizando el metodo contains', () =>{
+    it('Utilizando el metodo contains', () => {
         cy.visit('');
         cy.get('button#submitForm').contains('Register');
     });
 
-    xit('Utilizando el metodo contains', () =>{
+    it('Utilizando el metodo contains', () => {
         cy.visit('');
         cy.contains('White Pants').siblings('p#productPrice')
     });
+
+    it.only('Encontrando elemento web de forma parcial con *', () => {
+        cy.visit('');
+        cy.get("[class*='password']").should('exist'); //que contenga
+    })
+
+    it.only('Encontrando elemento web de forma parcial con ^', () => {
+        cy.visit('');
+        cy.get("[id^='use']").should('exist'); //que inicie
+    })
+
+    it.only('Encontrando elemento web de forma parcial con $', () => {
+        cy.visit('');
+        cy.get("[id$='ser']").should('exist'); //que finalice
+    })
+
+    it('Deberiamos encontrar el usuario en el header', () =>{
+        const usuario = 'fabrizio';
+        cy.get(`[id^='user_${usuario}_']`).should('exist');
+    })
 });
